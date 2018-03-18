8b6e2d122a1bf9582cf72e97bcbbaebdb5be408a
potykion <potykion@gmail.com>
2018-03-18 15:32:06 +0300

    Increase access token lifetime.

--- rptp/cookie.py
+++ rptp/cookie.py
@@ -1,5 +1,9 @@
+DAY_SECONDS = 3600 * 24
+
+
 def save_token_data(response, access_token, user_id=None):
     response.cookies['access_token'] = access_token
+    response.cookies['access_token']['max-age'] = DAY_SECONDS
 
     if user_id:
         response.cookies['user_id'] = str(user_id)

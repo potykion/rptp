770c660c20538800ad6d35158b6eed54553275ce
potykion <potykion@gmail.com>
2018-03-18 15:22:59 +0300

    Playing with envs

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from rptp.app import app
 
 if __name__ == '__main__':
     app.run(
-        os.getenv('HOST', 'localhost'),
-        os.getenv('PORT', '8000')
+        os.getenv('HOST', '0.0.0.0'),
+        int(os.getenv('PORT', '8000'))
     )

c5f3c23dc4d6e302f072a8a623577158c21a1cb9
potykion <potykion@gmail.com>
2018-06-24 16:02:17 +0300

    Log check_video_is_blocked

--- rptp/vk_api.py
+++ rptp/vk_api.py
@@ -1,4 +1,5 @@
 import asyncio
+import logging
 from urllib.parse import urlencode
 
 import aiohttp
@@ -10,16 +11,14 @@ async def request_videos(query, token, **kwargs):
     # https://vk.com/dev/api_requests
     # https://vk.com/dev/video.search
     params = {
-        'q': query,
-        'access_token': token,
-
-        'adult': 1,
-        'filters': 'mp4, long',
-        'hd': 1,
-        'count': 100,
-
-        'v': API_VERSION,
-        **kwargs
+        "q": query,
+        "access_token": token,
+        "adult": 1,
+        "filters": "mp4, long",
+        "hd": 1,
+        "count": 100,
+        "v": API_VERSION,
+        **kwargs,
     }
     encoded = urlencode(params)
     url = f"https://api.vk.com/method/video.search?{encoded}"
@@ -27,33 +26,31 @@ async def request_videos(query, token, **kwargs):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as result:
             response = await result.json()
-            return response['response']['items']
+            return response["response"]["items"]
 
 
 def generate_auth_link():
     params = {
-        'scope': 'video, offline',
-        'response_type': 'code',
-        'display': 'mobile',
-
-        'v': API_VERSION,
-        'client_id': APP_ID,
-        'redirect_uri': AUTH_REDIRECT_URI,
+        "scope": "video, offline",
+        "response_type": "code",
+        "display": "mobile",
+        "v": API_VERSION,
+        "client_id": APP_ID,
+        "redirect_uri": AUTH_REDIRECT_URI,
     }
     encoded = urlencode(params)
-    return f'https://oauth.vk.com/authorize?{encoded}'
+    return f"https://oauth.vk.com/authorize?{encoded}"
 
 
 async def request_token_data(code):
     params = {
-        'code': code,
-
-        'client_id': APP_ID,
-        'redirect_uri': AUTH_REDIRECT_URI,
-        'client_secret': CLIENT_SECRET,
+        "code": code,
+        "client_id": APP_ID,
+        "redirect_uri": AUTH_REDIRECT_URI,
+        "client_secret": CLIENT_SECRET,
     }
     encoded = urlencode(params)
-    url = f'https://oauth.vk.com/access_token?{encoded}'
+    url = f"https://oauth.vk.com/access_token?{encoded}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as result:
@@ -64,5 +61,19 @@ async def request_token_data(code):
 async def check_video_is_blocked(video_url):
     async with aiohttp.ClientSession() as session:
         async with session.get(video_url) as response:
+            logging.info(video_url)
+            logging.info(response.url)
+            logging.info(response.status)
+
             # 451 = Unavailable For Legal Reasons
             return response.status == 451
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    result = loop.run_until_complete(
+        check_video_is_blocked("https://vk.com/video-123841839_456239509")
+    )
+    loop.close()
+
+    s = "as"
